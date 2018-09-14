73b6609da1db30b11c8eb36cb78b48747a8041e0
Drew Ross <42723693+AndrewGallowitch@users.noreply.github.com>
2018-09-14 13:51:57 -0400

    New Load

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,114 @@
+
+// create an array of word options (just strings no spaces)
+var wordBank = [
+    "ronaldo", "pogba", "salah", "griezmann", "messi"];
+
+var images = [
+    "<img src= "];
+
+// Create global variables
+var wordIndex = "";
+var lettersInWordBank = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongGuesses = [];
+var letterGuessed = "";
+var imagecorrect = "";
+
+var wins = 0;
+var losses = 0;
+var guessNumber = 15;
+
+
+function startGame() {
+
+    uessNumber = 15;
+
+    // computer picks a random word from that array
+   
+    wordIndex = wordBank[Math.floor(Math.random() * wordBank.length)];
+    lettersInWordBank = wordIndex.split("");
+console.log(wordIndex)
+    numBlanks = lettersInWordBank.length;
+
+    blanksAndSuccesses = [];
+
+    wrongGuesses = [];
+
+    // loop over the chosen word to create blanks
+    for (var i = 0; i < numBlanks; i++) {
+        blanksAndSuccesses.push("_");
+    }
+
+    document.getElementById("guess-text").innerHTML = guessNumber;
+    document.getElementById("answer-text").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("wrong-text").innerHTML = wrongGuesses.join(" ");
+}
+
+// replace corresponding blank with the correct letter
+function checkLetters(letter) {
+    var letterInWord = false;
+
+    for (var i = 0; i < numBlanks; i++) {
+        if (wordIndex[i] === letter) {
+            letterInWord = true;
+        }
+    }
+    if (letterInWord) {
+        for (var j = 0; j < numBlanks; j++) {
+            if (wordIndex[j] === letter) {
+                blanksAndSuccesses[j] = letter;
+            }
+        }
+    }
+    else {
+        wrongGuesses.push(letter);
+        guessNumber--;
+    }
+}
+
+// restart game if user runs out of guesses/ move to next word when no blanks remain
+function roundComplete() {
+
+
+    document.getElementById("guess-text").innerHTML = guessNumber;
+    document.getElementById("answer-text").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("wrong-text").innerHTML = wrongGuesses.join(" ");
+
+
+    // compare user guess to letters in chosen word
+    // track incorrect guesses and guesses remaining; display to user
+    if (lettersInWordBank.toString() === blanksAndSuccesses.toString()) {
+        wins++;
+        $("#result").append()
+        document.getElementById("wins").innerHTML = "Wins: " + wins;
+
+        startGame();
+    }
+    else if (numGuesses === 0) {
+        losses++;
+        alert("You lose");
+        document.getElementById("losses").innerHTML = "Losses " + losses;
+
+        startGame();
+    }
+}
+
+startGame();
+
+// capture key events
+
+document.onkeyup = function (event) {
+
+    letterGuessed = String.fromCharCode(event.which).toLowerCase();
+
+    checkLetters(letterGuessed);
+
+    roundComplete();
+};
+
+
+
+
+
+
